77bf1f8cc8cefa4c213f7408fdae13fbba49e517
mat lurin <mat.lur518@gmail.com>
2023-01-07 20:23:17 +0100

    null Check + constante

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,13 +15,18 @@ namespace ConsoleApp1
 
         }
 
-        static void Formatage(ref string s)
+        static void Formatage(ref string chaineAFormater)
         {
+            if(chaineAFormater == null)
+            {
+                throw new ArgumentNullException("chaineAFormater");
+            }
+
             // Découper la chaîne de caractères en éléments séparés par des espaces
-            string[] elements = s.Split(' ');
+            string[] elements = chaineAFormater.Split(' ');
 
             // Vérifier si le deuxième élément est un opérateur
-            if (elements.Length > 1 && elements[1] == "+" || elements[1] == "-" || elements[1] == "*" || elements[1] == "/")
+            if ((elements.Length > 1) && (elements[1] == "+") || (elements[1] == "-") || (elements[1] == "*") || (elements[1] == "/"))
             {
                 // Intervertir la position de l'opérateur et de l'opérande suivant
                 string temp = elements[1];
@@ -30,19 +35,27 @@ namespace ConsoleApp1
             }
 
             // Reconstruire la chaîne de caractères à partir des éléments du tableau
-            s = String.Join(" ", elements);
+            chaineAFormater = String.Join(" ", elements);
         }
 
 
 
-        static int CalculateRPN(string s, Stack<int> stack)
+        static int CalculateRPN(string chaineACalculer, Stack<int> pileDeStockage)
             {
-           
+
+            if (chaineACalculer==null)
+            {
+                throw new ArgumentNullException("chaineACalculer");
+            }
+            else if(pileDeStockage == null)
+            {
+                throw new ArgumentNullException("pileDeStockage");
+            }
 
                 // Variable temporaire pour stocker les chiffres lors de la conversion en nombre
                 string temp = "";
                 // Parcours de la chaîne de caractères
-                foreach (char c in s)
+                foreach (char c in chaineACalculer)
                 {
                     // Si c'est un chiffre, ajout du chiffre à la chaîne temporaire
                     if (char.IsDigit(c))
@@ -55,7 +68,7 @@ namespace ConsoleApp1
                         // Si la chaîne temporaire n'est pas vide, conversion en nombre et ajout à la pile
                         if (temp != "")
                         {
-                            stack.Push(int.Parse(temp));
+                            pileDeStockage.Push(int.Parse(temp));
                             temp = "";
                         }
                     // Si c'est un opérateur, on récupère les deux derniers nombres de la pile et on effectue l'opération
@@ -65,25 +78,25 @@ namespace ConsoleApp1
 
                         
                         case '+':
-                             int n1 = stack.Pop();
-                             int n2 = stack.Pop();
-                            stack.Push(n1 + n2);
+                             int n1 = pileDeStockage.Pop();
+                             int n2 = pileDeStockage.Pop();
+                            pileDeStockage.Push(n1 + n2);
                             break;
                         case '-':
-                            int n3 = stack.Pop();
-                            int n4 = stack.Pop();
-                            stack.Push(n3 - n4);
+                            int n3 = pileDeStockage.Pop();
+                            int n4 = pileDeStockage.Pop();
+                            pileDeStockage.Push(n3 - n4);
 
                             break;
                         case '*':
-                            int n5 = stack.Pop();
-                            int n6 = stack.Pop();
-                            stack.Push(n5 * n6);
+                            int n5 = pileDeStockage.Pop();
+                            int n6 = pileDeStockage.Pop();
+                            pileDeStockage.Push(n5 * n6);
                             break;
                         case '/':
-                            int n7 = stack.Pop();
-                            int n8 = stack.Pop();
-                            stack.Push(n7 / n8);
+                            int n7 = pileDeStockage.Pop();
+                            int n8 = pileDeStockage.Pop();
+                            pileDeStockage.Push(n7 / n8);
                             break;
 
                     }
@@ -93,40 +106,40 @@ namespace ConsoleApp1
                 // Si la chaîne temporaire n'est pas vide, conversion en nombre et ajout à la pile
                 if (temp != "")
                 {
-                    stack.Push(int.Parse(temp));
+                    pileDeStockage.Push(int.Parse(temp));
                 }
 
                 // Si la pile ne contient qu'un seul élément, c'est le résultat du calcul
-                if (stack.Count == 1)
+                if (pileDeStockage.Count == 1)
                 {
-                    return stack.Peek();
+                    return pileDeStockage.Peek();
                 }
                 // Si la pile contient plusieurs éléments, cela signifie que la chaîne de caractères ne respecte pas la notation RPN
                 else
                 {
                     // On affiche un message d'erreur et on vide la pile
-                    if (stack.Count > 1)
+                    if (pileDeStockage.Count > 1)
                     {
                         Console.WriteLine("Erreur : chaîne de caractères non valide pour une notation RPN");
-                        stack.Clear();
+                        pileDeStockage.Clear();
                     }
                     // Si la pile ne contient qu'un élément, c'est le résultat du calcul RPN
-                    else if (stack.Count == 1)
+                    else if (pileDeStockage.Count == 1)
                     {
-                        return stack.Pop();
+                        return pileDeStockage.Pop();
                     }
                     // Si la pile est vide, il n'y a pas eu de calcul à effectuer
                     else
                     {
                         return 0;
                     }
-                return stack.Peek();
+                return pileDeStockage.Peek();
             }
             }
 
                     static void Main(string[] args)
         {
-            string titre = "Calcul RPI : ";
+           const string titre = "Calcul RPI : ";
 
 
             Stack<int> pileDeStockage = new Stack<int>();
