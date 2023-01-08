437914bb5df1ebecd81968ca10666c46063ac3f8
mat lurin <mat.lur518@gmail.com>
2023-01-08 14:39:31 +0100

    Ajout Message d'Erreur

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,103 +10,111 @@ namespace ConsoleApp1
         {
             Console.WriteLine(titre);
             string s;
-            s = Console.ReadLine();
+
+            do
+            {
+                s = Console.ReadLine();
+            } while (s == " ");
+ 
+
             return s;
 
         }
 
         static void Formatage(ref string chaineAFormater)
         {
-            if(chaineAFormater == null)
+            if (chaineAFormater == null)
             {
                 throw new ArgumentNullException("chaineAFormater");
             }
-
-            // Découper la chaîne de caractères en éléments séparés par des espaces
-            string[] elements = chaineAFormater.Split(' ');
-
-            // Vérifier si le deuxième élément est un opérateur
-            if ((elements.Length > 1) && (elements[1] == "+") || (elements[1] == "-") || (elements[1] == "*") || (elements[1] == "/"))
+            else
             {
-                // Intervertir la position de l'opérateur et de l'opérande suivant
-                string temp = elements[1];
-                elements[1] = elements[2];
-                elements[2] = temp;
-            }
+                // Découper la chaîne de caractères en éléments séparés par des espaces
+                string[] elements = chaineAFormater.Split(' ');
 
-            // Reconstruire la chaîne de caractères à partir des éléments du tableau
-            chaineAFormater = String.Join(" ", elements);
+                // Vérifier si le deuxième élément est un opérateur
+                if ((elements.Length > 1) && (elements[1] == "+") || (elements[1] == "-") || (elements[1] == "*") || (elements[1] == "/"))
+                {
+                    // Intervertir la position de l'opérateur et de l'opérande suivant
+                    string temp = elements[1];
+                    elements[1] = elements[2];
+                    elements[2] = temp;
+                }
+
+                // Reconstruire la chaîne de caractères à partir des éléments du tableau
+                chaineAFormater = String.Join(" ", elements);
+            }
         }
 
+        static int? CalculateRPN(string chaineACalculer, Stack<int> pileDeStockage)
+        {
 
-
-        static int CalculateRPN(string chaineACalculer, Stack<int> pileDeStockage)
+            if (chaineACalculer == null)
             {
-
-            if (chaineACalculer==null)
-            {
-                throw new ArgumentNullException("chaineACalculer");
+                Console.WriteLine("Erreur : La chaîne est nulle");
+                return null;
             }
-            else if(pileDeStockage == null)
+            else if (pileDeStockage == null)
             {
-                throw new ArgumentNullException("pileDeStockage");
+                Console.WriteLine("Erreur : Pas de Pile de Stockage");
+                return null;
             }
+            else if ((chaineACalculer != null) && (pileDeStockage != null))
+            {
 
                 // Variable temporaire pour stocker les chiffres lors de la conversion en nombre
-                string temp = "";
+                string variableTemporaireStockageChiffreConverti = "";
                 // Parcours de la chaîne de caractères
                 foreach (char c in chaineACalculer)
                 {
                     // Si c'est un chiffre, ajout du chiffre à la chaîne temporaire
                     if (char.IsDigit(c))
                     {
-                        temp += c;
+                        variableTemporaireStockageChiffreConverti += c;
                     }
                     // Si c'est un opérateur ou un espace, on traite la chaîne temporaire comme un nombre s'il y en a un, et on traite l'opérateur ou l'espace
                     else
                     {
                         // Si la chaîne temporaire n'est pas vide, conversion en nombre et ajout à la pile
-                        if (temp != "")
+                        if (variableTemporaireStockageChiffreConverti != "")
                         {
-                            pileDeStockage.Push(int.Parse(temp));
-                            temp = "";
+                            pileDeStockage.Push(int.Parse(variableTemporaireStockageChiffreConverti));
+                            variableTemporaireStockageChiffreConverti = "";
                         }
-                    // Si c'est un opérateur, on récupère les deux derniers nombres de la pile et on effectue l'opération
+                        // Si c'est un opérateur, on récupère les deux derniers nombres de la pile et on effectue l'opération
 
-                    switch (c)
-                    {
+                        switch (c)
+                        {
+                            case '+':
+                                int n1 = pileDeStockage.Pop();
+                                int n2 = pileDeStockage.Pop();
+                                pileDeStockage.Push(n1 + n2);
+                                break;
+                            case '-':
+                                int n3 = pileDeStockage.Pop();
+                                int n4 = pileDeStockage.Pop();
+                                pileDeStockage.Push(n3 - n4);
 
-                        
-                        case '+':
-                             int n1 = pileDeStockage.Pop();
-                             int n2 = pileDeStockage.Pop();
-                            pileDeStockage.Push(n1 + n2);
-                            break;
-                        case '-':
-                            int n3 = pileDeStockage.Pop();
-                            int n4 = pileDeStockage.Pop();
-                            pileDeStockage.Push(n3 - n4);
+                                break;
+                            case '*':
+                                int n5 = pileDeStockage.Pop();
+                                int n6 = pileDeStockage.Pop();
+                                pileDeStockage.Push(n5 * n6);
+                                break;
+                            case '/':
+                                int n7 = pileDeStockage.Pop();
+                                int n8 = pileDeStockage.Pop();
+                                pileDeStockage.Push(n7 / n8);
+                                break;
 
-                            break;
-                        case '*':
-                            int n5 = pileDeStockage.Pop();
-                            int n6 = pileDeStockage.Pop();
-                            pileDeStockage.Push(n5 * n6);
-                            break;
-                        case '/':
-                            int n7 = pileDeStockage.Pop();
-                            int n8 = pileDeStockage.Pop();
-                            pileDeStockage.Push(n7 / n8);
-                            break;
+                        }
 
-                    }
-                   
                     }
                 }
                 // Si la chaîne temporaire n'est pas vide, conversion en nombre et ajout à la pile
-                if (temp != "")
+                if (variableTemporaireStockageChiffreConverti != "")
                 {
-                    pileDeStockage.Push(int.Parse(temp));
+                    pileDeStockage.Push(int.Parse(variableTemporaireStockageChiffreConverti));
                 }
 
                 // Si la pile ne contient qu'un seul élément, c'est le résultat du calcul
@@ -122,6 +130,7 @@ namespace ConsoleApp1
                     {
                         Console.WriteLine("Erreur : chaîne de caractères non valide pour une notation RPN");
                         pileDeStockage.Clear();
+                        return null;
                     }
                     // Si la pile ne contient qu'un élément, c'est le résultat du calcul RPN
                     else if (pileDeStockage.Count == 1)
@@ -133,11 +142,16 @@ namespace ConsoleApp1
                     {
                         return 0;
                     }
-                return pileDeStockage.Peek();
+                    
+                }
+                
             }
-            }
+            return pileDeStockage.Peek();
+        }
 
-                    static void Main(string[] args)
+
+
+        static void Main(string[] args)
         {
            const string titre = "Calcul RPI : ";
 
@@ -148,10 +162,18 @@ namespace ConsoleApp1
 
             Formatage(ref calculRPI);
 
-            CalculateRPN(calculRPI, pileDeStockage);
+            Console.WriteLine(calculRPI);
 
-            Console.WriteLine($"Le resultat du calcul en notatation postfixé '{calculRPI}' est {pileDeStockage.Peek()}");
+           int? result =  CalculateRPN(calculRPI, pileDeStockage);
 
+            if (result != null)
+            {
+                Console.WriteLine($"Le resultat du calcul en notatation postfixé '{calculRPI}' est {pileDeStockage.Peek()}");
+            }
+            else
+            {
+                Console.WriteLine("Erreur : résultat nul");
+            }
             Console.ReadKey();
 
         }
